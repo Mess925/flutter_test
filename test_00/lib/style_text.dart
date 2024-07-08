989137761848bb3dd03d1378eb77154900462039
Han Min Thant <hanminthant222@gmail.com>
2024-07-08 15:19:43 +0800

    color_var

--- test_00/lib/style_text.dart
+++ test_00/lib/style_text.dart
@@ -10,7 +10,7 @@ class StyleText extends StatelessWidget {
     return Text(
       text,
       style: const TextStyle(
-        color: Color.fromARGB(255, 238, 255, 0),
+        color: Color.fromARGB(255, 55, 32, 234),
         fontSize: 18,
       ),
     );
